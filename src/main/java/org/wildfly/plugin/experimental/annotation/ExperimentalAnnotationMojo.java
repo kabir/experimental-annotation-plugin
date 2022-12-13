33b0780738cf44058bf6a9c6b9841436c3caa8b1
Kabir Khan <kkhan@redhat.com>
2022-12-13 20:57:28 +0000

    Add outputFile parameter to dump file

--- src/main/java/org/wildfly/plugin/experimental/annotation/ExperimentalAnnotationMojo.java
+++ src/main/java/org/wildfly/plugin/experimental/annotation/ExperimentalAnnotationMojo.java
@@ -28,13 +28,18 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.Index;
 import org.jboss.jandex.Indexer;
 import org.jboss.jandex.JarIndexer;
 import org.jboss.jandex.Result;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -53,38 +58,47 @@ public class ExperimentalAnnotationMojo
     extends AbstractMojo
 {
 
-    @Parameter(property = "filters")
+    @Parameter(property = "filters", required = true)
     List<Filter> filters = new ArrayList<>();
+
+    @Parameter(property = "outputFile", required = true)
+    File outputFile;
 
     @Component
     MavenProject mavenProject;
 
     Set<String> foundClasses = new HashSet<>();
 
-    public void execute()
-        throws MojoExecutionException
-    {
-        Log log = getLog();
-        log.info("Running plugin");
+    public void execute() throws MojoExecutionException {
+        try {
+            Log log = getLog();
+            log.info("Running plugin");
 
-        log.info(filters.toString());
+            log.info(filters.toString());
 
-        List<Dependency> dependencies = mavenProject.getDependencies();
+            List<Dependency> dependencies = mavenProject.getDependencies();
 
-        Set<String> allGroupIds = new HashSet<>();
-        for (Filter filter : filters) {
-            allGroupIds.addAll(filter.getGroupIds());
-        }
-
-        for (Artifact artifact : mavenProject.getArtifacts()) {
-            // log.info(artifact.getGroupId() + ":" + artifact.getArtifactId());
-            if (artifact.getType().equals("jar") && allGroupIds.contains(artifact.getGroupId())) {
-                searchExperimentalAnnotation(artifact);
+            Set<String> allGroupIds = new HashSet<>();
+            for (Filter filter : filters) {
+                allGroupIds.addAll(filter.getGroupIds());
             }
+
+            for (Artifact artifact : mavenProject.getArtifacts()) {
+                // log.info(artifact.getGroupId() + ":" + artifact.getArtifactId());
+                if (artifact.getType().equals("jar") && allGroupIds.contains(artifact.getGroupId())) {
+                    searchExperimentalAnnotation(artifact);
+                }
+            }
+
+            Path path = Paths.get(outputFile.toURI());
+            Files.createDirectories(path.getParent());
+            Files.write(path, foundClasses);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private void searchExperimentalAnnotation(Artifact artifact) {
+    private void searchExperimentalAnnotation(Artifact artifact) throws IOException {
         for (Filter filter : filters) {
             if (filter.getGroupIds().contains(artifact.getGroupId())) {
                 searchExperimentalAnnotation(artifact, filter);
@@ -92,32 +106,26 @@ public class ExperimentalAnnotationMojo
         }
     }
 
-    private void searchExperimentalAnnotation(Artifact artifact, Filter filter) {
+    private void searchExperimentalAnnotation(Artifact artifact, Filter filter) throws IOException {
         Indexer indexer = new Indexer();
-        try {
-            getLog().info("Indexing " + artifact.getFile());
-            Result result = JarIndexer.createJarIndex(artifact.getFile(), indexer, false, true, false);
-            Index index = result.getIndex();
+        getLog().info("Indexing " + artifact.getFile());
+        Result result = JarIndexer.createJarIndex(artifact.getFile(), indexer, false, true, false);
+        Index index = result.getIndex();
 
-            Collection<AnnotationInstance> annotations = index.getAnnotations(filter.getAnnotation());
+        Collection<AnnotationInstance> annotations = index.getAnnotations(filter.getAnnotation());
 
-            for (AnnotationInstance annotation : annotations) {
-                switch (annotation.target().kind()) {
-                    case CLASS:
-                        ClassInfo classInfo = annotation.target().asClass();
-                        if (!filter.getExcludedClasses().contains(classInfo.name().toString())) {
-                            boolean isAnn = classInfo.isAnnotation();
-                            if (isAnn) {
-                                boolean hasAnn = annotation.target().hasDeclaredAnnotation(filter.getAnnotation());
-                                System.out.println(classInfo.name().toString());
-                                foundClasses.add(classInfo.name().toString());
-                            }
-                        }
+        for (AnnotationInstance annotation : annotations) {
+            if (annotation.target().kind() == AnnotationTarget.Kind.CLASS) {
+                ClassInfo classInfo = annotation.target().asClass();
+                if (!filter.getExcludedClasses().contains(classInfo.name().toString())) {
+                    boolean isAnn = classInfo.isAnnotation();
+                    if (isAnn) {
+                        boolean hasAnn = annotation.target().hasDeclaredAnnotation(filter.getAnnotation());
+                        System.out.println(classInfo.name().toString());
+                        foundClasses.add(classInfo.name().toString());
+                    }
                 }
             }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
